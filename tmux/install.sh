9e791368f2749bd59d409ac2b6ec5c987e1def89
Ved Patel <vedmpatel@gmail.com>
2021-02-07 14:25:43 -0500

    Updated aliases and tmux conf

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,16 @@
 
 TYPE="$1"
 
-cp tmux.conf.$1 ~/.tmux.conf
+case $TYPE in
+    1404)
+        cp tmux.conf.1404 ~/.tmux.conf
+        ;;
+    1604)
+        cp tmux.conf.1604 ~/.tmux.conf
+        ;;
+    2004)
+        cp tmux.conf.2004 ~/.tmux.conf
+        ;;
+    powerline)
+        cp tmux.conf.powerline ~/.tmux.conf
+        ;;
